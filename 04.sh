57cee201982ba698ba1427f842c777edb428d840
Jash.Liao <jash.liao@gmail.com>
2025-08-22 12:55:23 +0800

    Create

---
+++ 04.sh
@@ -0,0 +1,6 @@
+cd part_01
+ls -1t --time=creation -r > ../list01.txt
+cd part_02
+ls -1t --time=creation -r > ../list02.txt
+cd part_03
+ls -1t --time=creation -r > ../list03.txt
---
+++ Readme.txt
@@ -0,0 +1,4 @@
+PAPAYA 電腦教室_Stable Diffusion(本地安裝開源免費的執行AI繪圖軟體) EP1~EP3
+
+HW要求:SSD(50G)+ RAM(16G)+ NVIDIA GeForce RTX 3060
+
